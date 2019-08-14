12592677047af578e046d61201e34b45d2081934
Steve Coluccio <thecalooch@gmail.com>
2019-08-14 08:44:47 -0400

    procedural terrain generation

--- shader_doodles_unity/Assets/_scripts/gauss/ProcGenMountain.cs
+++ shader_doodles_unity/Assets/_scripts/gauss/ProcGenMountain.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
-
+[ExecuteInEditMode]
 public class ProcGenMountain : MonoBehaviour
 {
     [SerializeField]
@@ -21,6 +21,15 @@ public class ProcGenMountain : MonoBehaviour
     [SerializeField]
     public float bellWidth;
 
+    [SerializeField]
+    public Vector3 scale = new Vector3(1,1,1);
+
+    private List<GameObject> m_currentBlocks;
+    private float m_height;
+    private float m_bellWidth;
+    private Vector3 m_origin;
+    private Vector3 m_blockDimensions;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,23 +40,45 @@ public class ProcGenMountain : MonoBehaviour
             o.SetActive(false);
         }
 
-        //spawn mountain blocks
-        Vector3 blockOrigin = blockDimentions / 2;
-        Quaternion zeroq = new Quaternion(0, 0, 0, 0);
-        for(int x = 0; x < blockDimentions.x; ++x)
+    }
+
+    public void Update()
+    {
+        if(m_height != height || m_bellWidth != bellWidth || m_origin != origin || m_blockDimensions != blockDimentions)
         {
-            for(int z=0; z < blockDimentions.z; ++z)
+            m_height = height;
+            m_bellWidth = bellWidth;
+            m_origin = origin;
+            m_blockDimensions = blockDimentions;
+
+            //cleanup the old blocks
+            foreach (GameObject obj in m_currentBlocks)
+                DestroyImmediate(obj);
+
+            m_currentBlocks.Clear();
+
+            _generate();
+        }   
+    }
+
+    /// <summary>
+    /// Generate moutiain
+    /// </summary>
+    private void _generate()
+    {
+        Vector3 blockOrigin = m_blockDimensions / 2;
+        Quaternion zeroq = new Quaternion(0, 0, 0, 0);
+        for (int x = 0; x < m_blockDimensions.x; ++x)
+        {
+            for (int z = 0; z < m_blockDimensions.z; ++z)
             {
-                Vector3 dp = new Vector3(x,0,z);
-                var gauss = _createGaussValue(height, bellWidth, dp, blockOrigin);
-                Debug.Log(gauss);
-                GameObject block = Instantiate(prefab, origin + dp - gauss / 2, zeroq);
-                block.transform.localScale = new Vector3(block.transform.localScale.x, block.transform.localScale.y * gauss.y, block.transform.localScale.z);
+                Vector3 dp = new Vector3(x, 0, z);
+                var gauss = _createGaussValue(m_height, m_bellWidth, dp, blockOrigin);
+                GameObject block = Instantiate(prefab, m_origin + dp - (gauss / 2), zeroq);
+                block.transform.localScale = new Vector3(scale.x, scale.y * gauss.y, scale.z);
+                m_currentBlocks.Add(block);
             }
         }
-
-
-        
     }
 
     /// <summary>
@@ -62,9 +93,13 @@ public class ProcGenMountain : MonoBehaviour
     private Vector3 _createGaussValue(float height, float width, Vector3 distFromOrigin, Vector3 origin)
     {
         Vector3 ret = new Vector3();
-        ret.y = 1 / (height * -2.71f * ( ((float)Math.Pow(distFromOrigin.x - origin.x,2.0) / (2 * (float)Math.Pow(width, 2.0))) + ((float)Math.Pow(distFromOrigin.z - origin.z, 2.0) / (2 * (float)Math.Pow(width, 2.0))) )) ;
+        ret.y = 1 / (height * -2.71f * 
+            ( ((float)Math.Pow(distFromOrigin.x - origin.x,2.0) / (2 * (float)Math.Pow(width, 2.0))) + 
+            ((float)Math.Pow(distFromOrigin.z - origin.z, 2.0) / (2 * (float)Math.Pow(width, 2.0))) )) ;
         return ret;
     }
+
+    
     
 
 }
---
+++ shader_doodles_unity/Assets/_scripts/terrain/TerrainGenerator.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    [SerializeField]
+    public int depth = 20;
+    [SerializeField]
+    public int width = 256;
+    [SerializeField]
+    public int height = 256;
+    [SerializeField]
+    public float scale = 20.0f;
+
+    private void Start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = _generateTerrain(terrain.terrainData);
+    }
+
+    private TerrainData _generateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, _generateHeights());
+        return terrainData;
+    }
+
+    private float[,] _generateHeights()
+    {
+        float[,] heights = new float[width, height];
+        for(int x=0; x<width; ++x)
+        {
+            for(int y=0; y<height; ++y)
+            {
+                heights[x, y] = _calculateHeight(x, y);
+            }
+        }
+        return heights;
+    }
+
+    private float _calculateHeight(float x, float y)
+    {
+        float xCoord = (x / width) * scale;
+        float yCoord = (y / height) * scale;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
